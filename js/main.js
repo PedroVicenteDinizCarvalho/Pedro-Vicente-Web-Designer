347c1ef566d0d9ad4217dfb40c6e8f9ff8a7ca9a
Pedro Vicente <pedrozwpiper@gmail.com>
2017-04-27 00:02:52 -0300

    Slider estilo JS Jquery Owl Carousel

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('#slider').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    autoHeight: true,
+    autoplayHoverPause: true,
+    autoplay: true,
+    autoplayTimeout:5000,
+})
+
